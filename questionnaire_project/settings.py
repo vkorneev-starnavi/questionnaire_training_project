cdb9f03aadd4ba69921e21dad4e6ff5e5b4b1314
Vyacheslav Korneev <vchslv13@ukr.net>
2019-07-04 17:54:11 +0300

    Use PostgreSQL in development

--- questionnaire_project/settings.py
+++ questionnaire_project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'questionnaire_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'questionnaire',
+        'USER': 'questionnaire_user',
+        'PASSWORD': 'questionnaire_passwd',
+        'HOST': '127.0.0.1'
     }
 }
 
