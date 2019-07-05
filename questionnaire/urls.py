61544505b0d7dbd620391806a93ccf60b484dec5
Vyacheslav Korneev <vchslv13@ukr.net>
2019-07-05 14:55:00 +0300

    Create CRUD views for QuestionnaireField

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -1,6 +1,16 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
-from questionnaire.views import QuestionnaireViewSet
+
+from questionnaire.views import QuestionnaireViewSet, \
+    QuestionnaireFieldCreate, QuestionnaireFieldDetail
 
 router = SimpleRouter()
 router.register('questionnaires', QuestionnaireViewSet)
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('questionnaire-fields/', QuestionnaireFieldCreate.as_view(),
+         name='questionnairefield-list'),
+    path('questionnaire-fields/<pk>/', QuestionnaireFieldDetail.as_view(),
+         name='questionnairefield-detail')
+]
+urlpatterns += router.urls
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,9 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView
 
-from questionnaire.models import Questionnaire
-from questionnaire.serializers import QuestionnaireSerializer
+
+from questionnaire.models import Questionnaire, QuestionnaireField
+from questionnaire.serializers import QuestionnaireSerializer, \
+    QuestionnaireFieldSerializer
 
 
 class QuestionnaireViewSet(ModelViewSet):
     serializer_class = QuestionnaireSerializer
     queryset = Questionnaire.objects.all()
+
+
+class QuestionnaireFieldCreate(CreateAPIView):
+    queryset = QuestionnaireField.objects.all()
+    serializer_class = QuestionnaireFieldSerializer
+
+
+class QuestionnaireFieldDetail(RetrieveUpdateDestroyAPIView):
+    queryset = QuestionnaireField.objects.all()
+    serializer_class = QuestionnaireFieldSerializer
