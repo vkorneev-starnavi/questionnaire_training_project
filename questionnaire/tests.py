f2617db6b4c3f5519848b5cf9f74b90188b5c034
Vyacheslav Korneev <vchslv13@ukr.net>
2019-07-08 04:06:46 +0300

    Implement questionnaire permissions

--- questionnaire/tests.py
+++ questionnaire/tests.py
@@ -11,13 +11,39 @@ class QuestionnaireTests(TestCase):
         self.client = APIClient()
         self.users = [
             User.objects.create_user(username='test1', password='test1',
-                                     email='test1@email.com')
+                                     email='test1@email.com'),
+            User.objects.create_user(username='test2', password='test2',
+                                     email='test2@email.com')
         ]
+
+    def test_unauthenticated_user_cant_create_questionnaire(self):
+        user_url = reverse('user-detail', kwargs={'pk': self.users[0].id})
+        questionnaire_data = {'author': user_url, 'description': 'Test'}
+        r = self.client.post(reverse('questionnaire-list'), questionnaire_data)
+        self.assertEqual(r.status_code, 401)
 
     def test_user_can_create_questionnaire(self):
         user_url = reverse('user-detail', kwargs={'pk': self.users[0].id})
         questionnaire_data = {'author': user_url, 'description': 'Test'}
-        r = self.client.post(reverse('questionnaire-list'), questionnaire_data)
 
-        assert r.status_code == 201
+        self.client.force_authenticate(self.users[0])
+        r = self.client.post(reverse('questionnaire-list'), questionnaire_data)
+        self.assertEqual(r.status_code, 201)
         Questionnaire.objects.get(pk=r.json()['id'])  # shouldn't raise
+
+    def test_user_cant_update_not_his_questionnaire(self):
+        user1_url = reverse('user-detail', kwargs={'pk': self.users[0].id})
+        questionnaire_data = {'author': user1_url, 'description': 'Test'}
+
+        # user1 creates new questionnaire
+        self.client.force_authenticate(self.users[0])
+        r = self.client.post(reverse('questionnaire-list'), questionnaire_data)
+        self.assertEqual(r.status_code, 201)
+
+        # user2 tries to change it
+        questionnaire_url = reverse('questionnaire-detail',
+                                    kwargs={'pk': r.json()['id']})
+        self.client.force_authenticate(self.users[1])
+        r = self.client.patch(questionnaire_url, {'description': 'Bad data'})
+        self.assertEqual(r.status_code, 403)
+
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, \
     CreateAPIView, ListCreateAPIView
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 from questionnaire.models import Questionnaire, QuestionnaireField, \
@@ -9,9 +11,24 @@ from questionnaire.serializers import QuestionnaireSerializer, \
     QuestionnaireFieldSerializer, QuestionnaireAnswerSerializer
 
 
+class IsSelfOrAdminToUpdateOrDelete(permissions.BasePermission):
+    message = "The user profile can be edited only by this user " \
+              "or site administrator."
+
+    def has_object_permission(self, request, view, questionnaire):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user and (questionnaire.author == request.user
+                             or request.user.is_staff):
+            return True
+        return False
+
+
 class QuestionnaireViewSet(ModelViewSet):
     serializer_class = QuestionnaireSerializer
     queryset = Questionnaire.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsSelfOrAdminToUpdateOrDelete)
 
 
 class QuestionnaireFieldCreate(CreateAPIView):
